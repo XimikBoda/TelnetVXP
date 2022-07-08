d180584d69a99574d88a94369a2f25c676ccbf27
XimikBoda <46135530+XimikBoda@users.noreply.github.com>
2022-07-08 21:00:27 +0300

    Init

---
+++ Packagedll.bat
@@ -0,0 +1,11 @@
+"C:\MRE_SDK\tools\DllPackage.exe" "D:\mreapp\TelnetVXP\TelnetVXP.vcproj"
+if %errorlevel% == 0 (
+ echo postbuild OK.
+  copy TelnetVXP.vpp ..\..\..\MoDIS_VC9\WIN32FS\DRIVE_E\TelnetVXP.vpp /y
+exit 0
+)else (
+echo postbuild error
+  echo error code: %errorlevel%
+  exit 1
+)
+
