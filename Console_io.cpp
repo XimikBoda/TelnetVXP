b489e93e54987960c9feeb0355f84cc61346aa78
XimikBoda <46135530+XimikBoda@users.noreply.github.com>
2022-07-18 20:37:29 +0300

    Added C support

--- Console_io.cpp
+++ Console_io.cpp
@@ -3,27 +3,27 @@ extern Console console;
 extern Telnet telnet;
 extern T2Input t2input;
 
-void console_char_in(char ch){
+extern "C" void console_char_in(char ch){
 	console.put_c(ch);
 }
 
-void console_str_in(const char* str){
+extern "C" void console_str_in(const char* str){
 	console.putstr(str);
 }
 
-void console_str_with_length_in(const char* str, int length){
+extern "C" void console_str_with_length_in(const char* str, int length){
 	console.putstr(str, length);
 }
 
-void console_char_out(char ch){
+extern "C" void console_char_out(char ch){
 	static char temp_str[2] = {ch, 0};
 	telnet.send_data(temp_str);
 }
 
-void console_str_out(const char* str){
+extern "C" void console_str_out(const char* str){
 	telnet.send_data((char*)str);
 }
 
-void console_str_with_length_out(const char* str, int length){
+extern "C" void console_str_with_length_out(const char* str, int length){
 	telnet.send_data((char*)str, length);
 }
--- Console_io.h
+++ Console_io.h
@@ -4,10 +4,10 @@
 #include "Telnet.h"
 #include "T2Input.h"
 
-void console_char_in(char ch);
-void console_str_in(const char* str);
-void console_str_with_length_in(const char* str, int length);
+extern "C" void console_char_in(char ch);
+extern "C" void console_str_in(const char* str);
+extern "C" void console_str_with_length_in(const char* str, int length);
 
-void console_char_out(char ch);
-void console_str_out(const char* str);
-void console_str_with_length_out(const char* str, int length);
+extern "C" void console_char_out(char ch);
+extern "C" void console_str_out(const char* str);
+extern "C" void console_str_with_length_out(const char* str, int length);
